b051d62994f523fd08af7bf3cb78a424a4bd7d4a
DanilPechkin <pppike15@gmail.com>
2021-02-07 21:33:45 +0500

    Added main activity_main.xml

--- app/src/main/java/com/example/danilp_notes/MainActivity.kt
+++ app/src/main/java/com/example/danilp_notes/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.danilp_notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener{
+
+        }
     }
 }
