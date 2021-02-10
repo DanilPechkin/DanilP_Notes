e2aaf4be9b905f63347409999b97931d71bc7072
DanilPechkin <pppike15@gmail.com>
2021-02-10 23:24:52 +0500

    Note saving now working.

--- app/src/main/java/com/example/danilp_notes/Edit.kt
+++ app/src/main/java/com/example/danilp_notes/Edit.kt
@@ -1,20 +1,62 @@
 package com.example.danilp_notes
 
-import android.annotation.SuppressLint
-import android.app.ActionBar
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.cardview.widget.CardView
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.card_edit.*
+import java.lang.reflect.Array
 
 class Edit : AppCompatActivity() {
+    lateinit var database: SQLiteDatabase
+    lateinit var mSettings: SharedPreferences
+    val APP_PREFERENCES: String = "mysettings"
+    val APP_PREFERENCES_ID: String = "PresentId"
+    lateinit var id: String
+    val selection = "_id =?"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.card_edit)
+
+        val dbHelper = DBHelper(this)
+        database = dbHelper.writableDatabase
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+
+        id = mSettings.getString(APP_PREFERENCES_ID, "").toString()
+
+        val selectionArgs = arrayOf(id)
+
+        val cursor: Cursor = database.query(DBHelper.TABLE_CONTACTS, null, selection,
+            selectionArgs, null, null, null)
+
+        if (cursor.moveToFirst()){
+            Header.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_HEADER)))
+            Note.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_NOTE)))
+        }
+        buttonBack.setOnClickListener{
+            onSave()
+        }
+        buttonDelete.setOnClickListener{
+            onDelete()
+        }
+    }
+    private fun onSave(){
+        val selectionArgs = arrayOf(id)
+        val contentValues = ContentValues()
+        contentValues.put(DBHelper.KEY_HEADER, Header.text.toString())
+        contentValues.put(DBHelper.KEY_NOTE, Note.text.toString())
+
+        database.update(DBHelper.TABLE_CONTACTS, contentValues, DBHelper.KEY_ID + "= ?", arrayOf(id));
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun onDelete(){
+
     }
 }
--- app/src/main/java/com/example/danilp_notes/MainActivity.kt
+++ app/src/main/java/com/example/danilp_notes/MainActivity.kt
@@ -1,27 +1,65 @@
 package com.example.danilp_notes
 
-import android.annotation.SuppressLint
-import android.app.ActionBar
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
     var layturn : Boolean = true
-    var num : Int = 0
+
+    var num : Int = 1
+
+    val APP_PREFERENCES : String = "mysettings"
+    val APP_PREFERENCES_ID : String = "PresentId"
+    lateinit var mSettings : SharedPreferences
+    lateinit var dbHelper: DBHelper
+    lateinit var database: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        dbHelper = DBHelper(this)
+        database = dbHelper.writableDatabase
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+
+        val cursor: Cursor = database.query(DBHelper.TABLE_CONTACTS, null,
+            null, null, null, null, null)
+
+        if(cursor.moveToFirst()){
+            do {
+                createNew(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_HEADER)),
+                cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_NOTE)))
+                num++
+            }while (cursor.moveToNext())
+        }
 
 
         button.setOnClickListener{
+            val contentValues = ContentValues()
+            contentValues.put(DBHelper.KEY_HEADER, "")
+            contentValues.put(DBHelper.KEY_NOTE, "")
+            database.insert(DBHelper.TABLE_CONTACTS, null, contentValues)
+            val editor = mSettings.edit()
+            editor.putString(APP_PREFERENCES_ID, num.toString())
+            editor.apply()
+            val intent = Intent(this, Edit::class.java)
+            startActivity(intent)
+
             createNew("Header","Your note")
         }
     }
@@ -48,11 +86,6 @@ class MainActivity : AppCompatActivity() {
         var cardlay = LinearLayout(this)
         cardlay.orientation = LinearLayout.VERTICAL
 
-        card.setOnClickListener{
-            val intent = Intent(this, Edit::class.java)
-            intent.putExtra("key", 1)
-            startActivity(intent)
-        }
 
         var header = TextView(this)
         header.text = headertext
@@ -63,9 +96,23 @@ class MainActivity : AppCompatActivity() {
         note.text = notetext
         note.setTextColor(Color.parseColor("#CCFCB759"))
         note.textSize = 16F
+
+        var numtext = TextView(this)
+        numtext.visibility = View.GONE
+        numtext.text = num.toString()
+
+        card.setOnClickListener{
+            val editor = mSettings.edit()
+            editor.putString(APP_PREFERENCES_ID, numtext.text.toString())
+            editor.apply()
+
+            val intent = Intent(this, Edit::class.java)
+            startActivity(intent)
+        }
         
         cardlay.addView(header)
         cardlay.addView(note)
+        cardlay.addView(numtext)
         card.addView(cardlay)
         
         lay.addView(card)
