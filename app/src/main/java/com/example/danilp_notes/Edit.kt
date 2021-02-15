421a4423b19ecc1171934cda37f11a8dd992e661
DanilPechkin <pppike15@gmail.com>
2021-02-15 23:33:39 +0500

    Added notification, but no possible to set time

--- app/src/main/java/com/example/danilp_notes/Edit.kt
+++ app/src/main/java/com/example/danilp_notes/Edit.kt
@@ -1,5 +1,9 @@
 package com.example.danilp_notes
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
@@ -13,67 +17,88 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.card_edit.*
 import java.lang.reflect.Array
 
 class Edit : AppCompatActivity() {
+    // DB
     lateinit var database: SQLiteDatabase
     lateinit var mSettings: SharedPreferences
+    // Preference that gets id
     val APP_PREFERENCES: String = "mysettings"
     val APP_PREFERENCES_ID: String = "PresentId"
     lateinit var id: String
+    // Selection for DB
     val selection = "_id =?"
+
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "channelID"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.card_edit)
 
+        // Init DB
         val dbHelper = DBHelper(this)
         database = dbHelper.writableDatabase
-        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
 
+        // Get id from preference
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
         id = mSettings.getString(APP_PREFERENCES_ID, "").toString()
 
+        // Selection for DB
         val selectionArgs = arrayOf(id)
 
+        // Get saved note
         val cursor: Cursor = database.query(DBHelper.TABLE_CONTACTS, null, selection,
             selectionArgs, null, null, null)
-
         if (cursor.moveToFirst()){
             Header.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_HEADER)))
             Note.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.KEY_NOTE)))
         }
 
+        // Set homebutton to actionbar
         val actionBar = supportActionBar
         actionBar!!.setHomeButtonEnabled(true)
         actionBar!!.setDisplayHomeAsUpEnabled(true)
         actionBar.title = ""
-        val colorDrawable = ColorDrawable(Color.parseColor("#CD660C"))
-        actionBar.setBackgroundDrawable(colorDrawable)
         actionBar.setHomeAsUpIndicator(R.drawable.backbutton)
     }
 
+    // Actionbar menu
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Select menu item
         when (item.itemId){
             R.id.action_delete -> {
                 onDelete()
                 return true
             }
             R.id.action_set_time -> {
+                setNotification(Header.text.toString(),Note.text.toString())
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    // Set actionbar menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    // Get homebutton click
     override fun onSupportNavigateUp(): Boolean {
         onSave()
         return true
     }
+
+    // Save note
     private fun onSave(){
         val selectionArgs = arrayOf(id)
         val contentValues = ContentValues()
@@ -85,6 +110,8 @@ class Edit : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
+    // Delete note
     private fun onDelete(){
         val selectionArgs = arrayOf(id)
 
@@ -92,5 +119,27 @@ class Edit : AppCompatActivity() {
 
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    fun setNotification(title: String, message: String) {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID,
+                "NOTE_CHANNEL",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "CHANNEL_FOR_SHOWING_NOTES"
+            mNotificationManager.createNotificationChannel(channel)
+        }
+        val mBuilder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.backbutton)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis() + 10000)
+
+        val intent = Intent(applicationContext, Edit::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        mBuilder.setContentIntent(pi)
+        mNotificationManager.notify(0, mBuilder.build())
     }
 }
--- app/src/main/java/com/example/danilp_notes/MainActivity.kt
+++ app/src/main/java/com/example/danilp_notes/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.SharedPreferences
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.View
@@ -18,14 +19,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
+    // Changing lay for spawning note
     var layturn : Boolean = true
-
+    // Sets card id textview
     var num : Int = 1
-
+    // Saves card id for Edit
     val APP_PREFERENCES : String = "mysettings"
     val APP_PREFERENCES_ID : String = "PresentId"
     lateinit var mSettings : SharedPreferences
+    // Db
     lateinit var dbHelper: DBHelper
     lateinit var database: SQLiteDatabase
 
@@ -33,10 +35,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initiate db
         dbHelper = DBHelper(this)
         database = dbHelper.writableDatabase
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
 
+        // Show saved notes
         val cursor: Cursor = database.query(DBHelper.TABLE_CONTACTS, null,
             null, null, null, null, null)
 
@@ -48,13 +52,16 @@ class MainActivity : AppCompatActivity() {
             }while (cursor.moveToNext())
         }
 
-
+        // Button creates new note
         button.setOnClickListener{
+            // id ++
             num++
+            // Create new note in DB
             val contentValues = ContentValues()
             contentValues.put(DBHelper.KEY_HEADER, "")
             contentValues.put(DBHelper.KEY_NOTE, "")
             database.insert(DBHelper.TABLE_CONTACTS, null, contentValues)
+            // Save id to get it in Edit
             val editor = mSettings.edit()
             editor.putString(APP_PREFERENCES_ID, num.toString())
             editor.apply()
@@ -62,15 +69,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             createNew("Header","Your note")
         }
-        val actionBar = supportActionBar
-        actionBar.title = "My notes"
+        // Action bar
+        supportActionBar?.hide()
     }
-
+    // Create new note on main
     private fun createNew(headertext : String, notetext : String){
+        // Select lay for note
         var lay: LinearLayout = if (layturn) findViewById(R.id.firstlay)
         else findViewById(R.id.secondlay)
         layturn = !layturn
 
+        // Create note
         var card = CardView(this)
         val cardParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
@@ -107,6 +116,7 @@ class MainActivity : AppCompatActivity() {
         numtext.visibility = View.GONE
         numtext.text = num.toString()
 
+        // Listener for Edit
         card.setOnClickListener{
             val editor = mSettings.edit()
             editor.putString(APP_PREFERENCES_ID, numtext.text.toString())
@@ -115,7 +125,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Edit::class.java)
             startActivity(intent)
         }
-        
+
+        // Add note to lay
         cardlay.addView(header)
         cardlay.addView(note)
         cardlay.addView(numtext)
